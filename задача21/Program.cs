bb76fd766882da1b8d079412fa0c7f2beb021391
muhammadkodir <muhammadkodyra@email.com>
2022-11-25 00:34:34 +0500

    end of ers 21

--- задача21/Program.cs
+++ задача21/Program.cs
@@ -1,4 +1,15 @@
-﻿Console.WriteLine("number:");
-var number = Convert.ToInt64(Console.ReadLine());
-var sum = number % 10;
-Console.WriteLine(sum);
+﻿Console.WriteLine("write_x1:");
+var x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write_y1:");
+var y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write_z1:");
+var z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write_x2:");
+var x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write_y2:");
+var y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write_z2:");
+var z2 = Convert.ToInt32(Console.ReadLine());
+
+var result = Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2));
+Console.WriteLine(result);
