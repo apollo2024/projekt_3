744cb0e3e511c3b784e0268ead7a8e63ab563645
muhammadkodir <muhammadkodyra@email.com>
2022-11-24 21:21:04 +0500

    hd

---
+++ задача19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("number:");
+var number = Convert.ToInt64(Console.ReadLine());
+if (number < 10000 || number > 99999)
+{
+    Console.WriteLine("number is uncorrect");
+}
+else
+{
+    var n = number;
+    var i = 0;
+    while (i <= 4)
+   {
+     var sum = n / 10000;
+    n = 10 * (number - (n / 10000) * 10000);
+    Console.Write(sum);
+    i++;
+   } 
+}
