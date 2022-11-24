82c5bf7f96d7dd6007af9c22b6f91c5bf3c0182b
muhammadkodir <muhammadkodyra@email.com>
2022-11-25 00:35:04 +0500

    end of ers 23

---
+++ задача23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("number:");
+var number = Convert.ToInt32(Console.ReadLine());
+var i = 1;
+while(i <= number)
+{
+    var cube = Math.Pow((i), 3);
+    Console.WriteLine(cube);
+    i++;
+}
